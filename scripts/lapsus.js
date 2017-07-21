9e8ecc72fc3b7b7751309586b7963bed04cd3e48
wcordero <wcordero@Simone>
2017-07-21 05:26:15 -0600

    lapsus

--- scripts/lapsus.js
+++ scripts/lapsus.js
@@ -22,10 +22,7 @@ function refresh(){
     setTimeout("refresh();",rate);
 }
 function ch_rate(r){document.getElementById("l_rate").innerHTML=rate=r;}
-function ch_interval(i){
-    interval=i<120?i:Math.round(Math.pow(i-120,1.57035))+120;
-    document.getElementById("l_interval").innerHTML=interval;
-}
+function ch_interval(i){document.getElementById("l_interval").innerHTML=interval=i<60?i:i<120?((i-60)*2)+60:i<180?((i-120)*4)+180:i<240?((i-180)*8)+420:Math.round(Math.pow(i-240,1.9297))+900;}
 function ch_shutter(s){
     shutter=1/Math.pow(2,(s/3));
     if(shutter<1) document.getElementById("l_shutter").innerHTML="1/"+Math.round((1/shutter)*100)/100;
