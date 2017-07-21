de4747ffae88500c6c96aa40a909c563d05d70de
wcordero <wcordero@Simone>
2017-07-21 16:45:06 -0600

    lapsus

--- index.php
+++ index.php
@@ -9,21 +9,47 @@
         <script src="scripts/jquery.mobile-1.4.5.min.js"></script>
     </head>
     <body>
-        <img id="last" src="last.jpg" height="400"/><br>
-        <button type="button" >start</button>
-        <button type="button" >stop</button>
-        Shutter(<span id="l_shutter"></span>s)<br>
-        <input type="range" name="shutter" id="shutter" value="18" min="-15" max="33" onchange="ch_shutter(this.value);"><br>
-        ISO(<span id="l_iso"></span>)<br>
-        <input type="range" name="iso" id="iso" value="0" min="-3" max="18" onchange="ch_iso(this.value);"><br>
-        Rotation(<span id="l_rot"></span>°)<br>
-        <input type="range" name="rot" id="rot" value="0" min="0" max="3" onchange="ch_rot(this.value);"><br>
-        Interval(<span id="l_interval"></span>s):<br>
-        <input type="range" name="interval" id="interval" value="1" min="0" max="300" onchange="ch_interval(this.value);"><br>
-        Refresh(<span id="l_rate"></span>ms):<br>
-        <input type="range" name="refresh" id="refresh" value="1000" min="100" max="5000" step="100" onchange="ch_rate(this.value);"><br>
-        <?php //echo "lapsus"; ?>
-        <!--<iframe id="ls" src="scripts/ls.php"></iframe>-->
+        <div class="all">
+            <div class="preview">
+                <img id="last" src="last.jpg" height="400"/><br>
+            </div>
+            <div class="content">
+                <div class="category">
+                    <div class="element">
+                        <button type="button" id="test" onclick="test();">test</button>
+                    </div>
+                    <div class="element">
+                        <button type="button" id="start" onclick="control();">start</button>
+                    </div>
+                </div>
+                <div class="category">
+                    <div class="element">
+                        Shutter(<span id="l_shutter"></span>s)<br>
+                        <input type="range" name="shutter" id="shutter" value="18" min="-15" max="33" onchange="ch_shutter(this.value);"><br>
+                    </div>
+                    <div class="element">
+                        ISO(<span id="l_iso"></span>)<br>
+                        <input type="range" name="iso" id="iso" value="0" min="-3" max="18" onchange="ch_iso(this.value);"><br>
+                    </div>
+                    <div class="element">
+                        Rotation(<span id="l_rot"></span>°)<br>
+                        <input type="range" name="rot" id="rot" value="0" min="0" max="3" onchange="ch_rot(this.value);"><br>
+                    </div>
+                    <div class="element">
+                        Interval(<span id="l_interval"></span>s):<br>
+                        <input type="range" name="interval" id="interval" value="1" min="0" max="300" onchange="ch_interval(this.value);"><br>
+                    </div>
+                    <div class="element">
+                        Refresh(<span id="l_rate"></span>ms):<br>
+                        <input type="range" name="refresh" id="refresh" value="1000" min="100" max="5000" step="100" onchange="ch_rate(this.value);"><br>
+                        <?php //echo "lapsus"; ?>
+                    </div>
+                </div>
+                <div class="category">
+                    <!--<iframe id="ls" src="scripts/ls.php"></iframe>-->
+                </div>
+            </div>
+        </div>
     </body>
     <script src="scripts/lapsus.js"></script>
 </html>
--- scripts/ls.php
+++ scripts/ls.php
@@ -1,9 +1,10 @@
 <?php
-$dir='/var/www/html/lapsus/DCIM/';
+require 'config.php';
 header("Cache-Control: no-store, no-cache, must-revalidate");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
-header('Content-Type: application/json');
 header("Access-Control-Allow-Headers", "Content-Type");
-echo json_encode(scandir($dir, SCANDIR_SORT_DESCENDING));
+header('Content-Type: application/json');
+header('Content-Disposition: filename="ls.json"');
+echo json_encode(scandir($dcim,SCANDIR_SORT_DESCENDING));
 
