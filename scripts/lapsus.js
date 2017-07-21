2ba97912a7b2dd510ce601ae24d95d3fee7a9e54
wcordero <wcordero@Simone>
2017-07-21 16:45:00 -0600

    lapsus

--- scripts/lapsus.js
+++ scripts/lapsus.js
@@ -2,11 +2,9 @@ var shutter,iso,rot,rate,interval;
 function refresh(){
     document.getElementById('last').src='last.jpg?r='+(Math.random()*100000);
 //    document.getElementById('ls').src='scripts/ls.php';
-    
-    $.getJSON("scripts/ls.json", function(json){
-        console.log(json); // this will show the info it in firebug console
-    });
-    
+//    $.getJSON("scripts/ls.json", function(json){
+//        console.log(json);
+//    });
     setTimeout("refresh();",rate);
 }
 function     ch_rate(r){document.getElementById("l_rate").innerHTML=rate=r;}
@@ -14,6 +12,14 @@ function ch_interval(i){document.getElementById("l_interval").innerHTML=interval
 function  ch_shutter(s){document.getElementById("l_shutter").innerHTML=(s>0?"1/":"")+Math.round(((shutter=1/Math.pow(2,(s/3)))<1?1/shutter:shutter)*100)/100;}
 function      ch_iso(n){document.getElementById("l_iso").innerHTML=Math.round(iso=Math.pow(2,(n/3))*100);}
 function      ch_rot(r){document.getElementById("l_rot").innerHTML=rot=r*90;}
+function control(){
+    console.log("START");
+}
+function test(){
+    console.log("TEST");
+}
+
+
 ch_rate(1000);
 ch_interval(1);
 ch_shutter(18);
