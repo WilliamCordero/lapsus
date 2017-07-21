a2c0a99ed75c498ed83d32952580126aa7e094fa
wcordero <wcordero@Simone>
2017-07-21 04:40:00 -0600

    lapsus

--- index.php
+++ index.php
@@ -4,14 +4,22 @@
         <meta charset="UTF-8">
         <title></title>
         <meta  name="viewport" content="width=device-width, initial-scale=1">
-        <link   rel="stylesheet" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
-        <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
-        <script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+        <link   rel="stylesheet" href="scripts/jquery.mobile-1.4.5.min.css">
+        <script src="scripts/jquery-1.12.4.min.js"></script>
+        <script src="scripts/jquery.mobile-1.4.5.min.js"></script>
     </head>
     <body>
         <img  id="last" src="last.jpg" height="400"/><br>
-        Refresh(ms):<br>
-        <input type="range" name="refresh" id="refresh" value="1000" min="125" max="10000" onchange="ch_rate(this.value);"><br>
+        Shutter(<span id="l_shutter"></span>s)<br>
+        <input type="range" name="shutter" id="shutter" value="18" min="-15" max="33" onchange="ch_shutter(this.value);"><br>
+        ISO(<span id="l_iso"></span>)<br>
+        <input type="range" name="iso" id="iso" value="0" min="-3" max="18" onchange="ch_iso(this.value);"><br>
+        Rotation(<span id="l_rot"></span>°)<br>
+        <input type="range" name="rot" id="rot" value="0" min="0" max="3" onchange="ch_rot(this.value);"><br>
+        Interval(<span id="l_interval"></span>s):<br>
+        <input type="range" name="interval" id="interval" value="1" min="0" max="300" onchange="ch_interval(this.value);"><br>
+        Refresh(<span id="l_rate"></span>ms):<br>
+        <input type="range" name="refresh" id="refresh" value="1000" min="100" max="5000" step="100" onchange="ch_rate(this.value);"><br>
         <?php //echo "lapsus"; ?>
     </body>
     <script src="scripts/lapsus.js"></script>
