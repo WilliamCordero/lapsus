c9938023d630d910ebc67fb5fbf8d2a822361faa
wcordero <wcordero@Simone>
2017-07-21 00:29:51 -0600

    lapsus

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
+        <meta  name="viewport" content="width=device-width, initial-scale=1">
+        <link   rel="stylesheet" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
+        <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+        <script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
     </head>
     <body>
-        Hello Lapsus 1 2
-        <?php
-        // put your code here
-        ?>
+        <img  id="last" src="last.jpg" height="400"/><br>
+        Refresh(ms):<br>
+        <input type="range" name="refresh" id="refresh" value="1000" min="125" max="10000" onchange="ch_rate(this.value);"><br>
+        <?php //echo "lapsus"; ?>
     </body>
+    <script src="scripts/lapsus.js"></script>
 </html>
--- scripts/lapsus.sh
+++ scripts/lapsus.sh
@@ -2,42 +2,42 @@
 
 COUNT=0
 CRR="0.067"
-SLP=3
-
-TIMEOUT=1
-QUALITY=100
-THUMB="none"
-#ISO=100
-#AWB="sun"
+SLP=1
 
 DEF_ARG="-dt --nopreview"
 
 [ $WIDTH   ]&&  WIDTH="--width $WIDTH"
 [ $HEIGHT  ]&& HEIGHT="--height $HEIGHT"
-[ $QUALITY ]&&QUALITY="--quality $QUALITY"
-[ $TIMEOUT ]&&TIMEOUT="--timeout $TIMEOUT"
-[ $THUMB   ]&&  THUMB="--thumb $THUMB"
+[ $QUALITY ]&&QUALITY="--quality $QUALITY"||QUALITY="--quality 100"
+[ $TIMEOUT ]&&TIMEOUT="--timeout $TIMEOUT"||TIMEOUT="--timeout 1"
+[ $THUMB   ]&&  THUMB="--thumb $THUMB"    ||  THUMB="--thumb none"
 
 [ $SHARP   ]&&  SHARP="--sharpness $SHARP"  #(-100 to 100)
 [ $CONTR   ]&&  CONTR="--contrast $CONTR"   #(-100 to 100)
 [ $BRIGH   ]&&  BRIGH="--brightness $BRIGH" #(-100 to 100)
 [ $SAT     ]&&    SAT="--saturation $SAT"   #(-100 to 100)
 
-[ $ISO     ]&&    ISO="-ISO $ISO" #ISO="100"
+[ $AWB     ]&&    AWB="-awb $AWB" || AWB="-awb sun" #off,auto,sun,cloud,shade,tungsten,fluorescent,incandescent,flash,horizon
+[ $ISO     ]&&    ISO="-ISO $ISO" || ISO="-ISO 100" 
 [ $SS      ]&&     SS="-ss "`echo "scale=6;1000000*($SS)" | bc | cut -d. -f1`
-[ $AWB     ]&&    AWB="-awb $AWB" #off,auto,sun,cloud,shade,tungsten,fluorescent,incandescent,flash,horizon
 
-[ ! $ODIR  ]&&   ODIR="/var/www/html/DCIM"
+[ $ROT     ]&&    ROT="--rotation $ROT" #"0-359"
+
+[ $LAST    ]&&   LAST="--latest $LAST"||LAST="/var/www/html/lapsus/last.jpg"
+[ ! $ODIR  ]&&   ODIR="/var/www/html/lapsus/DCIM"
 [ ! $OFILE ]&&  OFILE="lapsus_"`date +%H%M%S`
+
 #[ ! $ ]&&=""
+
+[ ! -d $ODIR ]&& mkdir $ODIR
 
 function take {
     #Adjust()
-    echo `date +%H.%M.%S.%N` `printf "%06d\n" $COUNT` $DEF_ARG $WIDTH $HEIGHT $QUALITY $TIMEOUT $THUMB $SHARP $CONTR $BRIGH $SAT $ISO $SS $AWB -o $ODIR/$OFILE`printf "_%06d.jpg" $COUNT`
+    echo `date +%H.%M.%S.%N` `printf "%06d\n" $COUNT` $DEF_ARG $WIDTH $HEIGHT $QUALITY $TIMEOUT $THUMB $SHARP $CONTR $BRIGH $SAT $ISO $SS $AWB $ROT $LAST -o $ODIR/$OFILE`printf "_%06d.jpg" $COUNT`
     raspistill $DEF_ARG\
         $WIDTH $HEIGHT $QUALITY $TIMEOUT $THUMB\
         $SHARP $CONTR $BRIGH $SAT\
-        $ISO $SS $AWB\
+        $ISO $SS $AWB $ROT $LAST\
         -o $ODIR/$OFILE`printf "_%06d.jpg" $COUNT`
 }
 #while : ; do
@@ -46,3 +46,4 @@ while [ $COUNT -lt 600  ] ; do
     sleep $SLP #`echo "scale=4;$SLP-$CRR" | bc`
     COUNT=$[$COUNT+1]
 done
+
