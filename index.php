71553316c1de0d531e683410c4179dd4b0ed5476
wcordero <wcordero@Simone>
2017-07-21 19:54:36 -0600

    lapsus

--- index.php
+++ index.php
@@ -4,43 +4,46 @@
         <meta charset="UTF-8">
         <title>lapsus</title>
         <meta  name="viewport" content="width=device-width, initial-scale=1">
-        <link   rel="stylesheet"  href="assets/style/jquery.mobile-1.4.5.min.css">
-        <script src="assets/js/jquery-1.12.4.min.js"></script>
-        <script src="assets/js/jquery.mobile-1.4.5.min.js"></script>
+        <!--<link   rel="stylesheet"  href="style/jquery.mobile-1.4.5.min.css">-->
+        <link   rel="stylesheet"  href="style/lapsus.css">
+        <script src="js/jquery-1.12.4.min.js"></script>
+        <!--<script src="js/jquery.mobile-1.4.5.min.js"></script>--> 
     </head>
     <body>
         <div class="all">
             <div class="preview">
-                <img id="last" src="last.jpg" height="400"/><br>
+                <img id="last" src="last.jpg" width="100%"/>
             </div>
             <div class="content">
-                <div class="category">
-                    <div class="element">
-                        <button type="button" id="test" onclick="test();">test</button>
-                    </div>
-                    <div class="element">
-                        <button type="button" id="start" onclick="control();">start</button>
+                <div class="controls">
+                    <div class="controls_row">
+                        <div class="controls_cell">
+                            <button type="button" id="test" onclick="test();">test</button>
+                        </div>
+                        <div class="controls_cell">
+                            <button type="button" id="start" onclick="control();">start</button>
+                        </div>
                     </div>
                 </div>
                 <div class="category">
                     <div class="element">
-                        Shutter(<span id="l_shutter"></span>s)<br>
+                        Shutter(<span id="l_shutter"></span>s)
                         <input type="range" name="shutter" id="shutter" value="18" min="-15" max="33" onchange="ch_shutter(this.value);"><br>
                     </div>
                     <div class="element">
-                        ISO(<span id="l_iso"></span>)<br>
+                        ISO(<span id="l_iso"></span>)
                         <input type="range" name="iso" id="iso" value="0" min="-3" max="18" onchange="ch_iso(this.value);"><br>
                     </div>
                     <div class="element">
-                        Rotation(<span id="l_rot"></span>°)<br>
+                        Rotation(<span id="l_rot"></span>°)
                         <input type="range" name="rot" id="rot" value="0" min="0" max="3" onchange="ch_rot(this.value);"><br>
                     </div>
                     <div class="element">
-                        Interval(<span id="l_interval"></span>s):<br>
+                        Interval(<span id="l_interval"></span>s):
                         <input type="range" name="interval" id="interval" value="1" min="0" max="300" onchange="ch_interval(this.value);"><br>
                     </div>
                     <div class="element">
-                        Refresh(<span id="l_rate"></span>ms):<br>
+                        Refresh(<span id="l_rate"></span>ms):
                         <input type="range" name="refresh" id="refresh" value="1000" min="100" max="5000" step="100" onchange="ch_rate(this.value);"><br>
                         <?php //echo "lapsus"; ?>
                     </div>
@@ -51,5 +54,5 @@
             </div>
         </div>
     </body>
-    <script src="assets/js/lapsus.js"></script>
+    <script src="js/lapsus.js"></script>
 </html>
